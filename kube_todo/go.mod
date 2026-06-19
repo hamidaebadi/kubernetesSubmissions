2f7a3b0395a5ef8842de1310354fa929aa331381
Hamid Aebadi <hamidebadi1996@yahoo.com>
2026-06-19 13:20:22 +0300

    Add todo codes and docker

---
+++ kube_todo/go.mod
@@ -0,0 +1,7 @@
+module kube_todo
+
+go 1.26.4
+
+require github.com/gorilla/mux v1.8.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ kube_todo/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux" // Updated import path
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	//Load the .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	// Create a new router
+	router := mux.NewRouter()
+
+	// Handle requests
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from Gorilla !"))
+	})
+
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = ":8080"
+	}
+
+	log.Printf("Starting server on port %s...", port)
+	err := http.ListenAndServe(port, router)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
